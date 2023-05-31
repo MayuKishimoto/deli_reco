52af63f0836935f6e3baa774b4e0c275b8c155b7
mayu_kishimoto <nhpw.29@gmail.com>
2023-05-31 13:46:48 +0900

    add: negotiations テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_31_040610) do
+ActiveRecord::Schema.define(version: 2023_05_31_044232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,21 @@ ActiveRecord::Schema.define(version: 2023_05_31_040610) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "negotiations", force: :cascade do |t|
+    t.date "negotiate_at", null: false
+    t.string "name", null: false
+    t.integer "selling_price", null: false
+    t.integer "wholesale_price", null: false
+    t.text "product_image"
+    t.text "explanation", null: false
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_negotiations_on_product_id"
+    t.index ["user_id"], name: "index_negotiations_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -72,6 +87,8 @@ ActiveRecord::Schema.define(version: 2023_05_31_040610) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "negotiations", "products"
+  add_foreign_key "negotiations", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "clients"
 end
