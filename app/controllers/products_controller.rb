a72825e6fa39068b14c0ee981eee88c777db827c
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-02 12:20:04 +0900

    add: productsコントローラの作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,57 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_product, only: %i[ show edit update destroy ]
+  
   def index
+    @products = Product.all
+  end
+
+  def show
+  end
+    
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+  
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_url(@product), notice: "Product was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_url(@product), notice: "Product was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy!
+
+    redirect_to products_url, notice: "Product was successfully destroyed."
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(
+      :assumed_name, :assumed_selling_price, :assumed_wholesale_price,
+      :start_on, :period,	:volume, :purpose, :concept,
+      :reference_image, :reference_image_cache, :status,
+      :client_id,	:category_id,	:application_status
+    )
   end
 end
