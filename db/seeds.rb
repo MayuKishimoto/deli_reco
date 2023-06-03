2b5dfc017cef19eb31a9b2b2e73c7f4ce1fd4d2f
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-03 09:40:52 +0900

    add: seedデータのgitignore解除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+  )
+  Task.create!(
+    title: "タイトル#{n + 1}",
+    content: "コンテント#{n + 1}",
+    user_id: 1
+  )
+  Label.create!(
+    name: "ラベル#{n + 1}"
+  )
+end
