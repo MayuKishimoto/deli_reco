b2f36bbbaf308c53a11cf763c49a5e6b9008b424
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-05 17:42:39 +0900

    update: prductsコントローラの修正

--- app/controllers/negotiations_controller.rb
+++ app/controllers/negotiations_controller.rb
@@ -1,44 +1,48 @@
 class NegotiationsController < ApplicationController
-  before_action :set_product, only: [:create, :edit, :update]
+  before_action :set_product, only: %i[create edit update]
+
+  def edit
+    @negotiation = @product.negotiations.find(params[:id])
+
+    respond_to do |format|
+      flash.now[:notice] = t("views.negotiations.messages.edit")
+      format.js { render :edit }
+    end
+  end
 
   def create
     @negotiation = @product.negotiations.build(negotiation_params)
     @negotiation.user_id = current_user.id
+
     respond_to do |format|
       if @negotiation.save
         format.js { render :index }
       else
-        format.html { redirect_to product_path(@product), notice: '投稿できませんでした...' }
+        format.html { redirect_to product_path(@product), notice: t("errors.messages.can_not_register") }
       end
-    end
-  end
-
-  def edit
-    @negotiation = @product.negotiations.find(params[:id])
-    respond_to do |format|
-      flash.now[:notice] = 'コメントの編集中'
-      format.js { render :edit }
     end
   end
 
   def update
     @negotiation = @product.negotiations.find(params[:id])
-      respond_to do |format|
-        if @negotiation.update(negotiation_params)
-          flash.now[:notice] = 'コメントが編集されました'
-          format.js { render :index }
-        else
-          flash.now[:notice] = 'コメントの編集に失敗しました'
-          format.js { render :edit }
-        end
+
+    respond_to do |format|
+      if @negotiation.update(negotiation_params)
+        flash.now[:notice] = t("views.products.messages.update")
+        format.js { render :index }
+      else
+        flash.now[:notice] = t("errors.messages.can_not_update")
+        format.js { render :edit }
       end
+    end
   end
 
   def destroy
     @negotiation = Negotiation.find(params[:id])
     @negotiation.destroy
+    
     respond_to do |format|
-      flash.now[:notice] = 'コメントが削除されました'
+      flash.now[:notice] = t("views.products.messages.destroy")
       format.js { render :index }
     end
   end
