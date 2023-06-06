0b2e5e6e9f00b2a5f55b1e77c44d45c9e0ad53df
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-06 22:19:51 +0900

    add: letter_openerの設定

--- app/mailers/negotiation_mailer.rb
+++ app/mailers/negotiation_mailer.rb
@@ -2,6 +2,6 @@ class NegotiationMailer < ApplicationMailer
   def negotiation_mail(negotiation)
     @negotiation = negotiation
 
-    mail to: @negotiation.email, subject: "お問い合わせの確認メール"
+    mail to: "nhpw@hotmail.co.jp", subject: "お問い合わせの確認メール"
   end
 end
