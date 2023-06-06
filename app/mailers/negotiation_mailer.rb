077d96da462c10c1b566e3feffc451d4ab71d00a
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-06 21:41:48 +0900

    add: gem 'letter_opener_web'のインストール

--- app/mailers/negotiation_mailer.rb
+++ app/mailers/negotiation_mailer.rb
@@ -1,2 +1,7 @@
 class NegotiationMailer < ApplicationMailer
+  def negotiation_mail(negotiation)
+    @negotiation = negotiation
+
+    mail to: @negotiation.email, subject: "お問い合わせの確認メール"
+  end
 end
