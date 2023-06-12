9b56c16ce98b18ac4b1c0ac83548b0dc4e720295
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-12 16:05:21 +0900

    update: 商談結果の表示を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,4 @@ module ApplicationHelper
   def default_img(image)
     image.presence || 'default.png'
   end
-
-  # def sales?
-  #   self.departments.map(&:name).include?("営業") 
-  # end
-
-  # def developer?
-  #   self.departments.map(&:name).include?("開発")
-  # end
 end
