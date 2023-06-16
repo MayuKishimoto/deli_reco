ef5ed87fa5a4af26cf3b5efab99b994fd32d29cf
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-16 15:18:51 +0900

    add: テストファイルの設定

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,16 @@ module DeliReco
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.load_defaults 6.1
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
