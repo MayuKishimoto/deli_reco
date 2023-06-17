3852f37470394d6ed255959da1b929ac75c1bedd
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-17 15:12:46 +0900

    add: homeコントローラの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  root 'products#index'
+  root "homes#index"
 
   devise_for :users
   devise_scope :user do
