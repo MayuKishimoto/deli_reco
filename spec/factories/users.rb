473df765b9e35abb7c4d68a242ec5bafd7ef3c05
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-18 13:45:26 +0900

    add: Userモデルのテスト作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "test" }
+    number { 1 }
+    email  { "test@example.com" }
+    password  { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Userモデルのバリデーションのテスト" do
+    let!(:user) { FactoryBot.create(:user) }
+    describe "登録確認" do
+      context "必要な情報が全て登録された場合" do
+        it "ユーザー登録ができる" do
+          expect(user).to be_valid
+        end
+      end
+    end
+
+    describe "nameカラムの確認" do
+      context "名前が登録されなかった場合" do
+        it "ユーザー登録ができない" do
+          user.name = ""
+          expect(user).not_to be_valid
+        end
+      end
+      context "名前の文字数が255以上だった場合" do
+        it "ユーザー登録ができない" do
+          user.name = "a" * 256
+          expect(user).not_to be_valid
+        end
+      end
+    end
+
+    describe "numberカラムの確認" do
+      context "社員番号が登録されなかった場合" do
+        it "ユーザー登録ができない" do
+          user.number = ""
+          expect(user).not_to be_valid
+        end
+      end
+      context "社員番号の文字数が10以上だった場合" do
+        it "ユーザー登録ができない" do
+          user.number = 10 ** 10
+          expect(user).not_to be_valid
+        end
+      end
+    end
+
+    describe "emailカラムの確認" do
+      context "メールアドレスが登録されなかった場合" do
+        it "ユーザー登録ができない" do
+          user.email = ""
+          expect(user).not_to be_valid
+        end
+      end
+      context "メールアドレスの文字数が255以上だった場合" do
+        it "ユーザー登録ができない" do
+          user.email = "a" * 256 + '@example.com'
+          expect(user).not_to be_valid
+        end
+      end
+      context "メールアドレスが規定の形式と異なった場合" do
+        it "ユーザー登録ができない" do
+          user.email = "a" * 20
+          expect(user).not_to be_valid
+        end
+      end
+      context "メールアドレスが既存のものと重複した場合" do
+        it "ユーザー登録ができない" do
+          second_user = User.new(
+            name: "test2",
+            number: 2,
+            email: "test@example.com",
+            password: "password2"
+          )
+          expect(second_user).not_to be_valid
+        end
+      end
+    end
+
+    describe "passwordカラムの確認" do
+      context "パスワードが登録されなかった場合" do
+        it "ユーザー登録ができない" do
+          user.password = ""
+          expect(user).not_to be_valid
+        end
+      end
+      context "パスワードの文字数が6以下だった場合" do
+        it "ユーザー登録ができない" do
+          user.password = "a" * 5
+          expect(user).not_to be_valid
+        end
+      end
+    end
+  end
 end
