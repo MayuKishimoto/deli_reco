df10df17936d5d5593601e1539803c67d2faf5ab
mayu_kishimoto <nhpw.29@gmail.com>
2023-06-19 12:52:53 +0900

    add: results_specの作成

--- spec/system/negotiations_spec.rb
+++ spec/system/negotiations_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe "Negotiations", type: :system do
   let!(:sales_user) { FactoryBot.create(:sales_user) }
   let!(:developer_user) { FactoryBot.create(:developer_user) }
   let!(:admin_user) { FactoryBot.create(:admin_user) }
-  let!(:product) { FactoryBot.create(:product, client: client, category: category,user: user ,application_status: 2) }
+  let!(:product) { FactoryBot.create(:product, client: client, category: category, user: user ,application_status: 2) }
   let!(:negotiation) { FactoryBot.create(:negotiation, user: sales_user, product: product) }
-  let!(:result) { FactoryBot.create(:result, user: developer_user, negotiation: negotiation) }
+  let!(:negotiation2) { FactoryBot.create(:negotiation, user: sales_user, product: product, name: "テスト") }
 
   describe "モデルのCRUD機能に対するテスト" do
     before do
@@ -31,7 +31,7 @@ RSpec.describe "Negotiations", type: :system do
           fill_in "店頭価格（税抜）", with: "100"
           fill_in "先方卸値（税抜）", with: "100"
           fill_in "商品説明", with: "テスト"
-          click_button "登録"
+          page.all("input[type=submit]")[1].click
           expect(page).to have_content "テスト"
         end
       end
@@ -64,29 +64,59 @@ RSpec.describe "Negotiations", type: :system do
   end
 
   describe "アクセス制限に関するテスト" do
-    before do
-      visit new_user_session_path
-      fill_in "Eメール", with: "test_sales@example.com"
-      fill_in "パスワード", with: "password"
-      click_button "ログイン"
-      click_link "商品情報一覧"
-      click_link "詳細", match: :first
+    describe "開発のアクセス制限の確認" do
+      before do
+        visit new_user_session_path
+        fill_in "Eメール", with: "test_developer@example.com"
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+        click_link "商品情報一覧"
+        click_link "詳細", match: :first
+      end
+
+      context "開発が商談情報の詳細ページに飛んだ場合" do
+        it "商談情報の登録フォームが表示される" do
+          sleep 0.5
+          expect(page).to have_content "商談情報登録"
+        end
+
+        it "商談情報の編集ボタンが表示される" do
+          sleep 0.5
+          expect(page).to have_link("negotiation_edit")
+        end
+
+        it "商談情報の削除ボタンが表示される" do
+          sleep 0.5
+          expect(page).to have_link("negotiation_delete")
+        end
+      end
     end
 
-    context "営業が商談情報の詳細ページに飛んだ場合" do
-      it "商談情報の登録フォームが表示されない" do
-        sleep 0.5
-        expect(page).not_to have_content "商談情報登録"
+    describe "営業のアクセス制限の確認" do
+      before do
+        visit new_user_session_path
+        fill_in "Eメール", with: "test_sales@example.com"
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+        click_link "商品情報一覧"
+        click_link "詳細", match: :first
       end
 
-      it "商談情報の編集ボタンが表示されない" do
-        sleep 0.5
-        expect(page).not_to have_link("negotiation_edit")
-      end
+      context "営業が商談情報の詳細ページに飛んだ場合" do
+        it "商談情報の登録フォームが表示されない" do
+          sleep 0.5
+          expect(page).not_to have_content "商談情報登録"
+        end
 
-      it "商談情報の削除ボタンが表示されない" do
-        sleep 0.5
-        expect(page).not_to have_link("negotiation_delete")
+        it "商談情報の編集ボタンが表示されない" do
+          sleep 0.5
+          expect(page).not_to have_link("negotiation_edit")
+        end
+
+        it "商談情報の削除ボタンが表示されない" do
+          sleep 0.5
+          expect(page).not_to have_link("negotiation_delete")
+        end
       end
     end
   end
--- spec/system/results_spec.rb
+++ spec/system/results_spec.rb
@@ -7,13 +7,86 @@ RSpec.describe "Results", type: :system do
   let!(:sales_user) { FactoryBot.create(:sales_user) }
   let!(:developer_user) { FactoryBot.create(:developer_user) }
   let!(:admin_user) { FactoryBot.create(:admin_user) }
-  let!(:product) { FactoryBot.create(:product, client: client, category: category,user: user) }
+  let!(:product) { FactoryBot.create(:product, client: client, category: category, user: user, application_status: 2) }
   let!(:negotiation) { FactoryBot.create(:negotiation, user: sales_user, product: product) }
+  let!(:negotiation2) { FactoryBot.create(:negotiation, user: sales_user, product: product) }
   let!(:result) { FactoryBot.create(:result, user: developer_user, negotiation: negotiation) }
 
-  before do
-    driven_by(:rack_test)
+  describe "モデルのCRUD機能に対するテスト" do
+    before do
+      visit new_user_session_path
+      fill_in "Eメール", with: "test_admin@example.com"
+      fill_in "パスワード", with: "password"
+      click_button "ログイン"
+      click_link "商品情報一覧"
+      click_link "詳細", match: :first
+    end
+
+    describe "#create" do
+      context "必要な情報が全て入力された場合" do
+        it "商談結果の登録ができる" do
+          fill_in "商談結果", with: "テスト"
+          page.all("input[type=submit]")[2].click
+          expect(page).to have_content "テスト"
+        end
+      end
+    end
+
+    describe "#update" do
+    let!(:result) { FactoryBot.create(:result, user: developer_user, negotiation: negotiation) }
+      context "既存の商談結果を編集した場合" do
+        it "更新した内容の登録ができる" do
+          
+          click_link "result_edit", match: :first
+          fill_in "商談結果", with: "テスト２"
+          page.all("input[type=submit]")[3].click
+          expect(page).to have_content "テスト２"
+        end
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "アクセス制限に関するテスト" do
+    describe "営業のアクセス制限の確認" do
+      before do
+        visit new_user_session_path
+        fill_in "Eメール", with: "test_sales@example.com"
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+        click_link "商品情報一覧"
+        click_link "詳細", match: :first
+      end
+
+      context "営業が商談結果の詳細ページに飛んだ場合" do
+        it "商談結果の登録フォームが表示される" do
+          expect(page).to have_css "#result_content"
+        end
+
+        it "商談結果の編集ボタンが表示される" do
+          expect(page).to have_link("result_edit")
+        end
+      end
+    end
+
+    describe "開発のアクセス制限の確認" do
+      before do
+        visit new_user_session_path
+        fill_in "Eメール", with: "test_developer@example.com"
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+        click_link "商品情報一覧"
+        click_link "詳細", match: :first
+      end
+
+      context "開発が商談結果の詳細ページに飛んだ場合" do
+        it "商談結果の登録フォームが表示されない" do
+          expect(page).not_to have_css "#result_content"
+        end
+
+        it "商談結果の編集ボタンが表示されない" do
+          expect(page).not_to have_link("result_edit")
+        end
+      end
+    end
+  end
 end
