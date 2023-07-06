917cb1e261ba3b8987a55ab82a183e5e68977d03
mayu_kishimoto <nhpw.29@gmail.com>
2023-07-06 18:34:40 +0900

    update: seedデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -352,16 +352,9 @@ ClientInCharge.create!([
 ])
 
 ProductInCharge.create!([
-  {user_id: 4, product_id: 2},
-  {user_id: 5, product_id: 4},
-  {user_id: 6, product_id: 6},
-  {user_id: 7, product_id: 8},
-  {user_id: 8, product_id: 10}
+  {user_id: 9, product_id: 2},
+  {user_id: 10, product_id: 4},
+  {user_id: 11, product_id: 6},
+  {user_id: 12, product_id: 8},
+  {user_id: 13, product_id: 10}
 ])
-
-# 10.times do |n|
-#   ProductInCharge.create!(
-#     user_id: 2,
-#     product_id: 1 + n
-#   )
-# end
